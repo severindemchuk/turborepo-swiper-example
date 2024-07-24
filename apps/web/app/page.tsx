c10095d3998eeefa512ac7719caf0be77137cba8
Severin Demchuk <severin.demchuk@dreipol.ch>
2024-07-24 11:21:50 +0200

    Add slider component with swiper dependency

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import { Slider } from "@repo/ui/slider";
 
 export default function Home() {
   return (
@@ -20,7 +21,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
+        <Slider />
         <div className={styles.ctas}>
           <a
             className={styles.primary}
